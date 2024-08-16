6f494a415e1b356d7eb98fc0fa5c16bf5d8813e9
Cawdroch <77812106+Cawdroch@users.noreply.github.com>
2024-08-16 09:51:21 +0100

    Added pusher console logging

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import React, { useEffect, useState } from "react";
+import Pusher from "pusher-js";
+
+Pusher.logToConsole = true;
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const pusher = new Pusher("9b9a0160120f08368632b", { cluster: "eu" });
+    
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      setMessages([...messages, data]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app__body">
